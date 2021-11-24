ea291c4723aa0180c6d08b13ef40b490b9255ef4
Lukas Beranek <beranek.lukas@gmail.com>
2021-11-24 16:00:51 +0100

    add darwin build

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env sh
-# This script is used in the README and https://www.infracost.io/docs/#quick-start
+# This script is used in the README and https://www.mh.io/docs/#quick-start
 set -e
 
 os=$(uname | tr '[:upper:]' '[:lower:]')
 arch=$(uname -m | tr '[:upper:]' '[:lower:]' | sed -e s/x86_64/amd64/)
-echo "Downloading latest release of infracost-$os-$arch..."
-curl -sL https://github.com/infracost/infracost/releases/latest/download/infracost-$os-$arch.tar.gz | tar xz -C /tmp
+name="mh_linux_$os_$arch"
+echo "Downloading latest release from https://github.com/modulehub/mh/releases/latest/download/$name.tar.gz..."
+curl -sL https://github.com/modulehub/mh/releases/latest/download/$name.tar.gz | tar xz -C /tmp
 echo
-echo "Moving /tmp/infracost-$os-$arch to /usr/local/bin/infracost (you might be asked for your password due to sudo)"
-sudo mv /tmp/infracost-$os-$arch /usr/local/bin/infracost
+echo "Moving /tmp/$name to /usr/local/bin/mh (you might be asked for your password due to sudo)"
+sudo mv /tmp/$name /usr/local/bin/mh
 echo
-echo "Completed installing $(infracost --version)"
+echo "Completed installing $(mh --version)"
