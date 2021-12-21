6b682e59d094fd7a08d983022f4c271f70b5fa32
Lukas Beranek <beranek.lukas@gmail.com>
2021-12-21 14:42:24 +0100

    fix installer path

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,11 +4,11 @@ set -e
 
 os=$(uname | tr '[:upper:]' '[:lower:]')
 arch=$(uname -m | tr '[:upper:]' '[:lower:]' | sed -e s/x86_64/amd64/)
-name="mh"
+name="mh_${os}_${arch}"
 url="https://github.com/modulehub/mh/releases/latest/download/$name.tar.gz"
 echo "Downloading latest release from ${url}..."
 curl -Lo ./mh.tar.gz ${url} || exit 1
 tar -xzf mh.tar.gz || exit 1
-chmod +x $name || exit 1
-mv $name /usr/local/mh || exit 1
+chmod +x mh || exit 1
+mv mh /usr/local/bin/mh || exit 1
 echo "Completed installing $(mh --version)"
